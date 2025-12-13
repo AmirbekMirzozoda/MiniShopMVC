9f842020df3011fb96760375383fffe718632ff0
amirbekmirzozoda <amirbekmirzozoda@gmail.com>
2025-12-13 20:04:47 +0500

    add status property to Order model

--- MiniShopMVC/Models/Entities/Order.cs
+++ MiniShopMVC/Models/Entities/Order.cs
@@ -5,6 +5,7 @@ public class Order : BaseEntity
     public DateTime OrderDate { get; set; }
     public string CustomerName { get; set; } = null!;
     public string CustomerPhone { get; set; } = null!;
+    public int Status { get; set; }
     
     public ICollection<ProductOrder> ProductOrders { get; set; } = null!;
 }
